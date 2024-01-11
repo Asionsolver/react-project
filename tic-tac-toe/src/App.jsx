d2e82d9c7c0695f3e12cfca08d0e019500349a67
asionsolver <asionsolver274@gmail.com>
2024-01-12 00:19:02 +0600

    Finally complete Tic-Tac-Toe all features successfully

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -89,21 +89,43 @@ function Board({ xIsNext, squares, onPlay }) {
 export default function Game() {
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [xIsNext, setXIsNext] = useState(true);
+  const [currentMove, setCurrentMove] = useState(0);
 
-  const currentSquares = history[history.length - 1];
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
     setXIsNext(!xIsNext);
-    setHistory([...history, nextSquares]);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
   }
 
+  function jumpTo(move){
+    setCurrentMove(move);
+    setXIsNext(move % 2 === 0);
+  }
+
+  const moves = history.map((squares, move)=>{
+    let description;
+    if(move > 0){
+      description = `Go to move #${move}`;
+    }else{
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button className="p-3 m-1  bg-purple-500 text-white rounded" onClick={()=>jumpTo(move)}>{description} </button>
+      </li>
+    )
+  })
+
   return (
-    <div>
+    <div className="flex justify-center gap-10">
       <div>
         <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
       </div>
-      <div>
-        <ol></ol>
+      <div className="bg-purple-200 mt-10 p-5 text-lg rounded-md">
+        <ol>{moves}</ol>
       </div>
     </div>
   );
