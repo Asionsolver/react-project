2d19cc2c6947fdadc35083c5615f0c4007fe6aa6
asionsolver <asionsolver274@gmail.com>
2024-01-11 22:40:51 +0600

    add some functionalities tic-tac-toe name like implement interactive and logically functional  button where user choice their favorite character like "X" or "O"

--- tic-tac-toe/src/App.jsx
+++ tic-tac-toe/src/App.jsx
@@ -1,13 +1,55 @@
+import { useState } from "react";
 
-
-
-export default function App() {
-  
+// eslint-disable-next-line react/prop-types
+function Square({ value, onSquareClick }) {
   return (
-    <div className="App">
-      <h1 className='text-3xl underline font-bold text-center'>Tic-Tac-Toe</h1>
-    </div>
-  )
+    <button
+      className="h-20 w-20 m-1 bg-purple-500 border  leading-9 text-white text-5xl rounded"
+      onClick={onSquareClick}
+    >
+      {value}{" "}
+    </button>
+  );
 }
 
- 
+export default function Board() {
+  const [squares, setSquares] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
+  function handleClick(i) {
+    const nextSquares = squares.slice();
+
+    if(squares[i]){
+      return;
+    }
+
+    if(xIsNext){
+      nextSquares[i] = "X";
+    }else{
+      nextSquares[i] = "O";
+    }
+
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  return (
+    <>
+      <div className="flex">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+
+      <div className="flex">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="flex">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
+  );
+}
