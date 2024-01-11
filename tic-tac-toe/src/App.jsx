aa628e9b8ccf1c898c24accf51ca12ed731c03a2
asionsolver <asionsolver274@gmail.com>
2024-01-11 12:50:48 +0600

    Create new project name like tic tac toe

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,13 @@
+
+
+
+export default function App() {
+  
+  return (
+    <div className="App">
+      <h1 className='text-3xl underline font-bold text-center'>Tic-Tac-Toe</h1>
+    </div>
+  )
+}
+
+ 
